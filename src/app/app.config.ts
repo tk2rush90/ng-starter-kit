cf0799998ed277b244b8a09c39d18d1463856bee
tk2rush90 <tk2rush90@naver.com>
2024-05-18 20:19:49 +0900

    feat: Install prettier and reformat codes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+  ],
 };
--- src/app/services/api/api-service.ts
+++ src/app/services/api/api-service.ts
@@ -1,5 +1,5 @@
-import {HttpClient} from '@angular/common/http';
-import {inject} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 
 /** Abstract class to create API services */
 export abstract class ApiService {
@@ -10,8 +10,5 @@ export abstract class ApiService {
    * Create API service.
    * @param host - API host to be used as prefix.
    */
-  protected constructor(
-    public readonly host: string,
-  ) {
-  }
+  protected constructor(public readonly host: string) {}
 }
