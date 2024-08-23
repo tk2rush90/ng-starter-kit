387af48a8432343f81180adea42319347eb8a399
tk2rush90 <tk2rush90@naver.com>
2024-08-24 01:10:14 +0900

    feat(modal): change default visual of modal

--- src/app/components/common/modal/modal.component.ts
+++ src/app/components/common/modal/modal.component.ts
@@ -3,17 +3,19 @@ import { fadeInOut } from '../../../animations/fade-in-out';
 import { ClickDetectorService } from '../../../services/app/click-detector/click-detector.service';
 import { OverlayService } from '../../../services/app/overlay/overlay.service';
 import { scaleUpDown } from '../../../animations/scale-up-down';
+import { IconButtonDirective } from '../icon-button/icon-button.directive';
+import { IconXMarkComponent } from '../../icons/icon-x-mark/icon-x-mark.component';
 
 /** A modal container */
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [IconButtonDirective, IconXMarkComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
   host: {
     'class':
-      'fixed bg-white rounded-lg p-4 flex-col-stretch shadow-lg top-1/2 left-1/2 pointer-events-auto max-h-[calc(100vh-32px)] overflow-auto',
+      'fixed bg-white rounded-2xl p-6 flex-col-stretch gap-5 shadow-lg top-1/2 left-1/2 pointer-events-auto max-h-[calc(100vh-32px)] overflow-auto w-full max-w-screen-xs',
     // To bind `transform` animation or TailwindCSS classes, use `translate` attribute.
     '[style.translate]': `'-50% -50%'`,
     '[@modalFadeInOut]': '',
@@ -22,8 +24,14 @@ import { scaleUpDown } from '../../../animations/scale-up-down';
   animations: [fadeInOut('modalFadeInOut'), scaleUpDown('modalScaleUpDown')],
 })
 export class ModalComponent implements AfterContentInit, OnDestroy {
+  /** Set modal title */
+  @Input() modalTitle = '';
+
   /** Set to keep opening on outside click */
   @Input({ transform: booleanAttribute }) keepOnOutsideClick = false;
+
+  /** Set to display close button */
+  @Input({ transform: booleanAttribute }) displayClose = false;
 
   constructor(
     private readonly _elementRef: ElementRef<HTMLElement>,
@@ -44,5 +52,10 @@ export class ModalComponent implements AfterContentInit, OnDestroy {
 
   ngOnDestroy() {
     this._clickDetectorService.cancelDetect(this._elementRef.nativeElement);
+  }
+
+  /** Close modal */
+  close(): void {
+    this._overlayService.closeByElement(this._elementRef.nativeElement);
   }
 }
