75a4df42b57d1de61cf88f87c11ec8338d8f75fa
tk2rush90 <tk2rush90@naver.com>
2024-10-23 10:22:16 +0900

    feat: modify image loader

--- src/app/components/common/image-loader/image-loader.directive.ts
+++ src/app/components/common/image-loader/image-loader.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostBinding, HostListener, OnDestroy } from '@angular/core';
+import { Platform } from '../../../utils/platform';
 
 /**
  * A directive to load image element.
@@ -8,13 +9,22 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
   selector: 'img[appImageLoader]',
   standalone: true,
   host: {
-    class: 'transition-opacity',
+    '[class.transition-opacity]': 'transition',
+    '[class.hidden]': 'imageError',
   },
   exportAs: 'imageLoader',
 })
-export class ImageLoaderDirective {
+export class ImageLoaderDirective implements AfterViewInit, OnDestroy {
   /** Image successfully loaded status */
   imageLoaded = false;
+
+  imageError = false;
+
+  transition = false;
+
+  private _mutationObserver?: MutationObserver;
+
+  constructor(private readonly _elementRef: ElementRef<HTMLImageElement>) {}
 
   /** Bind `opacity` according to image loaded status */
   @HostBinding('style.opacity')
@@ -22,14 +32,38 @@ export class ImageLoaderDirective {
     return this.imageLoaded ? 1 : 0;
   }
 
+  ngAfterViewInit() {
+    if (Platform.isBrowser) {
+      this._mutationObserver = new MutationObserver((records) => {
+        records.forEach((_record) => {
+          if (_record.attributeName?.toLowerCase() === 'src') {
+            this.imageLoaded = false;
+            this.imageError = false;
+            this.transition = false;
+          }
+        });
+      });
+
+      this._mutationObserver.observe(this._elementRef.nativeElement, {
+        attributes: true,
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this._mutationObserver?.disconnect();
+  }
+
   /** Listen `load` event of `img` element. It marks `imageLoaded` as `true` */
   @HostListener('load')
   onImageLoad(): void {
+    this.transition = true;
     this.imageLoaded = true;
   }
 
   @HostListener('error')
   onImageError(): void {
+    this.imageError = true;
     this.imageLoaded = false;
   }
 }
