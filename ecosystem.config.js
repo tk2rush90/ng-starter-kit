67fa490b300fd9b97c5a17ece123ee705a1fa326
tk2rush90 <tk2rush90@naver.com>
2024-11-16 21:35:29 +0900

    chore(pm2): fix script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ng-starter-kit',
-      script: 'node dist/ng-starter-kit/server/server.mjs',
+      script: 'node ~/Desktop/builds/ng-starter-kit/ng-starter-kit/server/server.mjs',
       instances: 1,
       env: {
         NODE_ENV: 'production',
